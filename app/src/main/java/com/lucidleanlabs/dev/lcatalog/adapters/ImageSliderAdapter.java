4b64e59f4a2b98df10ee071faf70db2997c73731
Raveendra B Aripaka <ravee.lll@outlook.com>
2017-04-26 19:45:30 +0530

    Width, Length and Height of the Objects Problem : Appending images to the product view page because of the JSON Array modifications

--- app/src/main/java/com/lucidleanlabs/dev/lcatalog/adapters/ImageSliderAdapter.java
+++ app/src/main/java/com/lucidleanlabs/dev/lcatalog/adapters/ImageSliderAdapter.java
@@ -10,17 +10,20 @@ import android.widget.ImageView;
 import com.lucidleanlabs.dev.lcatalog.R;
 
 import java.util.ArrayList;
+
 public class ImageSliderAdapter extends PagerAdapter {
 
     private ArrayList<Integer> Images;
     private LayoutInflater inflater;
     private Context context;
-    public ImageSliderAdapter(Context context, ArrayList<Integer> Images){
+
+    public ImageSliderAdapter(Context context, ArrayList<Integer> Images) {
         this.context = context;
         this.Images = Images;
         inflater = LayoutInflater.from(context);
 
     }
+
     @Override
     public int getCount() {
         return Images.size();
@@ -28,21 +31,22 @@ public class ImageSliderAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return (view==object);
+        return (view == object);
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container,int position){
-    inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater.inflate(R.layout.activity_product_page,container,false);
-        ImageView images= (ImageView)v.findViewById(R.id.article_image_view);
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_product_page, container, false);
+        ImageView images = (ImageView) v.findViewById(R.id.article_image_view);
         images.setImageResource(Images.get(position));
         container.addView(v);
         return v;
 
     }
+
     @Override
-    public void destroyItem(ViewGroup container,int position,Object object){
+    public void destroyItem(ViewGroup container, int position, Object object) {
         container.invalidate();
     }
 }
